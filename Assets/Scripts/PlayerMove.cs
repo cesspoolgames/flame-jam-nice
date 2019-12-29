64c3c1d3be3eac1708f51258e2c36cbd47ae05ed
Amir Eldor <amir@eize.ninja>
2019-12-29 12:45:36 +0200

    Player placeholder and Shefi's example

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 4;
+
+    void FixedUpdate()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        var movement = new Vector3(horizontal, vertical, 0);
+
+        movement = movement.normalized * speed;
+        transform.position += movement * Time.deltaTime;
+    }
+}
