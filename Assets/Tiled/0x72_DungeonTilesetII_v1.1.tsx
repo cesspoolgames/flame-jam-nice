682df4e8144846622b2c693f6c033f62f8978c9c
Amir Eldor <amir@eize.ninja>
2019-12-29 14:48:45 +0200

    Add sample tiled tilemap

--- Assets/Tiled/0x72_DungeonTilesetII_v1.1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="0x72_DungeonTilesetII_v1.1" tilewidth="1" tileheight="1" tilecount="262144" columns="512">
- <image source="0x72_DungeonTilesetII_v1.1.png" width="512" height="512"/>
-</tileset>
---
+++ Assets/Tiled/GameJam-map-final-sofi/GameJam-map/Dungeon_Character.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Dungeon_Character" tilewidth="16" tileheight="16" tilecount="28" columns="7">
+ <image source="2D Pixel Dungeon Asset Pack v2.0/2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Character.png" width="112" height="64"/>
+</tileset>
