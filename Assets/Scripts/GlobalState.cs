8501385a06d319b99c69c97f768ab1f115a58818
Amir Eldor <amir@eize.ninja>
2019-12-29 15:35:21 +0200

    Add first music asset (not using) + hit areas getting hit only when near + global state

---
+++ Assets/Scripts/GlobalState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalState : MonoBehaviour
+{
+
+    private static GameJamTable currentTable;
+    public static GameJamTable CurrentTable
+    {
+        set { currentTable = value; } // so we can have more control later on
+        get { return currentTable; }
+    }
+}
