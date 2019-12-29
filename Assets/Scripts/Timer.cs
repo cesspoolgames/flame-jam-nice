c449f1758c0c6a15e1a2f448c8e7a9259caaee2e
Michael Ostrovsky <micha.ostrov@gmail.com>
2019-12-29 13:18:27 +0200

    Adding script to control time

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft = 30.0f;  
+    public float scrollBar = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = scrollBar;
+        timeLeft -= Time.deltaTime;
+        TextMeshProUGUI mText = gameObject.GetComponent<TextMeshProUGUI>();
+        mText.text = timeLeft.ToString();
+    }
+}
