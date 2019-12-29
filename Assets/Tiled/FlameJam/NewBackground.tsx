00f474b27e52a7f8e0690dfddc2d3ac82e2ee5ab
Amir Eldor <amir@eize.ninja>
2019-12-29 18:09:18 +0200

    Add bad Tiled tilemap

---
+++ Assets/Tiled/FlameJam/NewBackground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="NewBackground" tilewidth="16" tileheight="16" tilecount="35" columns="7">
+ <image source="1x (2)/1x/background.png" width="112" height="80"/>
+</tileset>
