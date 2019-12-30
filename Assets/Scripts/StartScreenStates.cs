a27fd197b589b4dd3956f320f8e28bc38a52c4ad
Michael Ostrovsky <micha.ostrov@gmail.com>
2019-12-30 17:23:44 +0200

    Adding screens

---
+++ Assets/Scripts/StartScreenStates.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreenStates : MonoBehaviour
+{
+    public Sprite[] imageOptions;
+    private int state = 0;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (state < 2 && Input.anyKeyDown)
+        {
+            gameObject.GetComponent<Image>().sprite = imageOptions[state];
+            state += 1;
+        }
+    }
+}
