9c531e5aaad5e23020adb0236c85cbad3acca40f
Amir Eldor <amir@eize.ninja>
2019-12-30 11:13:35 +0200

    KeepTheFlameBar script

---
+++ Assets/Scripts/KeepTheFlameBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepTheFlameBar : MonoBehaviour
+{
+    private static KeepTheFlameBar instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
