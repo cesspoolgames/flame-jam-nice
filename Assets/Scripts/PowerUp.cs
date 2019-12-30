c8b1c446285ca4b9f81d4af87862e50af409572b
Michael Ostrovsky <micha.ostrov@gmail.com>
2019-12-30 19:08:24 +0200

    Powerup clip

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    public AudioSource powerupAudioSource;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
+        powerupAudioSource = GameObject.FindWithTag("AudioSourcePowerup").GetComponent<AudioSource>();
+        powerupAudioSource.PlayOneShot(powerupAudioSource.clip);
         GameObject.FindWithTag("Player").GetComponent<PlayerMove>().GivePowerUp();
         Destroy(gameObject);
     }
