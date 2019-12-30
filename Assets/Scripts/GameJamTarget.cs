448b4b91ef17db1095aefdd7cc07c0e0f1d2ff7f
Michael Ostrovsky <micha.ostrov@gmail.com>
2019-12-30 20:00:17 +0200

    Adding flamed sounds

--- Assets/Scripts/GameJamTarget.cs
+++ Assets/Scripts/GameJamTarget.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameJamTarget : MonoBehaviour
 {
     const float flameCap = 10f; // can't flame over this value
+    public AudioSource badFlameSound;
+    public AudioSource goodFlameSound;
+    public AudioSource totallyFlamedSound;
     private float flame = 0f; // decreases over time
     private Transform tilePointer;
     private bool flamed = false; // when flamed is reached, keeps the flamed state until cooled down
@@ -18,6 +21,9 @@ public class GameJamTarget : MonoBehaviour
     {
         tilePointer = transform.Find("TilePointer");
         TilemapTileChanger.ResetTile(tilePointer.position);
+        badFlameSound = GameObject.FindWithTag("AudioSourceFlamedBad").GetComponent<AudioSource>();
+        goodFlameSound = GameObject.FindWithTag("AudioSourceFlamedGood").GetComponent<AudioSource>();
+        totallyFlamedSound = GameObject.FindWithTag("AudioSourceTotallyFlamed").GetComponent<AudioSource>();
     }
 
     public void GetHit(float howMuch)
@@ -30,6 +36,15 @@ public class GameJamTarget : MonoBehaviour
             flame = flameCap;
             TilemapTileChanger.SetFlamed(tilePointer.position);
             flamed = true;
+            totallyFlamedSound.PlayOneShot(totallyFlamedSound.clip);
+        }
+        if (flamed)
+        {
+            badFlameSound.PlayOneShot(badFlameSound.clip);
+        }
+        else
+        {
+            goodFlameSound.PlayOneShot(goodFlameSound.clip);
         }
     }
 
